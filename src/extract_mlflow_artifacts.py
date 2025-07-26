c519b2780516ffa40913baa8167bc7ef183f162f
OnurKerimoglu <kerimoglu.o@gmail.com>
2025-07-26 14:28:03 +0200

    extract_mlflow_artifacts:  added step: upload_to_gcs; now extracts to 'extracted_model'; adapted Dockerfile and Makefile

--- src/extract_mlflow_artifacts.py
+++ src/extract_mlflow_artifacts.py
@@ -5,6 +5,9 @@ import os
 import mlflow
 from mlflow.tracking import MlflowClient
 
+from src.gcp_functions import clear_gcs_folder, upload_directory
+from src.load_configs import Configs
+
 # Global parameters
 mlflow.set_tracking_uri("http://127.0.0.1:5000")
 CLIENT = MlflowClient()
@@ -13,13 +16,18 @@ REGISTRY_NAME = "stocks_forecasting_candidates"  # The registry from which the m
 MODEL_ALIAS = "champion"
 
 rootpath = os.path.dirname(os.path.dirname(__file__))
-DEPLOYPATH = os.path.join(rootpath, "deployment")
+LOCALPATH = os.path.join(rootpath, "extracted_model")
 
 
-def main_extract_model() -> None:
+def main_extract_model(cloudupload: bool = True) -> None:
     run_id, params = retrieve_registered_model()
-    store_model_artifacts(run_id, params)
-    print("model parameters are extracted into: ", DEPLOYPATH)
+    store_model_artifacts_local(run_id, params)
+    print("model parameters are extracted into: ", LOCALPATH)
+    if cloudupload:
+        configs = Configs()
+        upload_to_gcs(configs.cloud, localpath=LOCALPATH)
+    else:
+        print("model artifacts are not uploaded to GCS")
 
 
 def retrieve_registered_model() -> tuple:
@@ -32,19 +40,19 @@ def retrieve_registered_model() -> tuple:
     return run_id, params
 
 
-def store_model_artifacts(run_id: str, params: dict) -> None:
-    # clean deployments_folder
-    if os.path.exists(DEPLOYPATH):
-        for f in os.listdir(DEPLOYPATH):
+def store_model_artifacts_local(run_id: str, params: dict) -> None:
+    # clean the local folder
+    if os.path.exists(LOCALPATH):
+        for f in os.listdir(LOCALPATH):
             try:
-                os.remove(os.path.join(DEPLOYPATH, f))
+                os.remove(os.path.join(LOCALPATH, f))
             except Exception as e:
                 print(e)
                 os.rmdir(f)
     else:
-        os.makedirs(DEPLOYPATH)
+        os.makedirs(LOCALPATH)
     # store params as json
-    params_fpath = os.path.join(DEPLOYPATH, "params.json")
+    params_fpath = os.path.join(LOCALPATH, "params.json")
     with open(params_fpath, "w") as f:
         json.dump(params, f)
     print("params are stored in: ", params_fpath)
@@ -53,23 +61,53 @@ def store_model_artifacts(run_id: str, params: dict) -> None:
     artifact_uri = f"runs:/{run_id}/{MODEL_ARTIFACT_FOLDER}"
     print(f"Downloading model and requirements from: {artifact_uri}")
     reqs_path_original = mlflow.artifacts.download_artifacts(
-        artifact_uri=f"{artifact_uri}/requirements.txt", dst_path=DEPLOYPATH
+        artifact_uri=f"{artifact_uri}/requirements.txt", dst_path=LOCALPATH
     )
     model_path_original = mlflow.artifacts.download_artifacts(
-        artifact_uri=f"{artifact_uri}/model.pkl", dst_path=DEPLOYPATH
+        artifact_uri=f"{artifact_uri}/model.pkl", dst_path=LOCALPATH
     )
-    reqs_path = os.path.join(DEPLOYPATH, "requirements.txt")
-    # move the requirements.txt and model.pkl to the deployments folder
-    os.rename(reqs_path_original, os.path.join(DEPLOYPATH, "requirements.txt"))
-    os.rename(model_path_original, os.path.join(DEPLOYPATH, "model.pkl"))
-    os.rmdir(os.path.join(DEPLOYPATH, MODEL_ARTIFACT_FOLDER))
+    reqs_path = os.path.join(LOCALPATH, "requirements.txt")
+    # move the requirements.txt and model.pkl to the local folder
+    os.rename(reqs_path_original, os.path.join(LOCALPATH, "requirements.txt"))
+    os.rename(model_path_original, os.path.join(LOCALPATH, "model.pkl"))
+    os.rmdir(os.path.join(LOCALPATH, MODEL_ARTIFACT_FOLDER))
     print("requirements.txt and model.pkl are stored in: ", reqs_path)
 
     # log date
-    date_fpath = os.path.join(DEPLOYPATH, "extraction_date.txt")
+    date_fpath = os.path.join(LOCALPATH, "extraction_date.txt")
     with open(date_fpath, "w") as f:
         f.write(datetime.datetime.now().isoformat())
 
 
+def upload_to_gcs(configs: dict, localpath: str) -> None:
+    """
+    Syncing a local directory to a GCS Bucket.
+
+    Parameters:
+        localpath (str): Path to the local directory.
+        configs (dict): Configuration dict with GCS project and bucket.
+    """
+    if not os.path.isdir(localpath):
+        raise FileNotFoundError(f"Local directory '{localpath}' does not exist.")
+
+    project_id = configs["gcs"]["project"]
+    bucket_name = configs["gcs"]["models_bucket"]
+    gcs_folder = os.path.basename(localpath.rstrip("/")) + "/"
+
+    clear_gcs_folder(project_id, bucket_name, gcs_folder)
+    upload_directory(project_id, bucket_name, gcs_folder, localpath)
+
+
 if __name__ == "__main__":
-    main_extract_model()
+    import argparse
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--cloudupload",
+        type=bool,
+        required=False,
+        help="upload the model artifacts to cloud",
+        default=False,
+    )
+    args = parser.parse_args()
+    main_extract_model(cloudupload=args.cloudupload)
